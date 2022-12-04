fb2bfb373a4dc04d7fa458fcc2061de804560e4e
steven69 <steven62885@gmail.com>
2022-12-04 18:51:52 +0800

    Initial Commit

---
+++ ios_final_00857039/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_final_00857039
+//
+//  Created by User20 on 2022/12/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_final_00857039/ios_final_00857039App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_final_00857039App.swift
+//  ios_final_00857039
+//
+//  Created by User20 on 2022/12/4.
+//
+
+import SwiftUI
+
+@main
+struct ios_final_00857039App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
